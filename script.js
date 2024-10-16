1adba9d57ba90c726b2a95d202369918f15161d0
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-16 20:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student = {
+	name: "Aditya Sharma",
+}
+
+Object.prototype.getKeys = function () {
+	return Object.keys(this);
+}
+
+console.log(student.getKeys());
